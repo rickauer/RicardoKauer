314616a401abe70c000181e2e60567bfae469ccc
rickauer <116601341+rickauer@users.noreply.github.com>
2023-10-05 18:12:31 -0300

    Update decremento.vhd

--- ULA/decremento.vhd
+++ ULA/decremento.vhd
@@ -1,33 +1,9 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:32:28 10/18/2022 
--- Design Name: 
--- Module Name:    decremento - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+--- Operação Decremento ---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity decremento is -- será feito o decremento de 1 bit
     Port ( cin : in  STD_LOGIC; -- bit de entrada, carry in
