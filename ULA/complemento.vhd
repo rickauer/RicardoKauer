2115156f0ab5c3a53f08a97f2ff694295eab53ab
rickauer <116601341+rickauer@users.noreply.github.com>
2023-10-05 18:12:57 -0300

    Update complemento.vhd

--- ULA/complemento.vhd
+++ ULA/complemento.vhd
@@ -1,34 +1,8 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:17:20 10/18/2022 
--- Design Name: 
--- Module Name:    complemento - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+--- Operação Complemento
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 
 entity complemento is -- aqui fazemos o complemento a dois de um vetor 
     Port ( cin : in  STD_LOGIC; -- bit de entrada, Carry In
