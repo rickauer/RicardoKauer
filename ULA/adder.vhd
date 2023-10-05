d4afe775b6ea7690b1df68a49031fb241d568076
rickauer <116601341+rickauer@users.noreply.github.com>
2023-10-05 18:13:21 -0300

    Update adder.vhd

--- ULA/adder.vhd
+++ ULA/adder.vhd
@@ -1,33 +1,8 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:47:36 09/20/2022 
--- Design Name: 
--- Module Name:    adder - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+--- Somador de 1 bit ---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity adder is --somador bit a bit 
     Port ( x, y, z: in  STD_LOGIC; --aqui definimos as entradas, sendo x e y os bits a serem somados e z o carry in
